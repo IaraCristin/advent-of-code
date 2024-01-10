695515af13ba50ea4dd775372583f6d7efc84ccf
Tu Nombre <you@example.com>
2024-01-10 13:41:48 -0300

    me rendí a python

---
+++ 2023/Day 5/IYGASF.py
@@ -0,0 +1,14 @@
+with open("example.txt") as f:
+    s = f.read().strip()
+
+s = s.split('\n')
+seeds = 
+
+
+""" 
+Es solo una nota para mi esto
+Esto es para sacar los elementos vacíos de la lista 
+s = list(filter(None,s))
+"""
+
+print(s)
--- 2023/Day 5/If_You_Give_A_Seed_A_Fertilizer.js
+++ 2023/Day 5/If_You_Give_A_Seed_A_Fertilizer.js
@@ -8,12 +8,12 @@ import { start } from 'repl';
 //Leemos el archivo y lo separamos por cada salto de línea
 //Trim() es para sacar espacios en blanco que nos queden sin querer
 //Y lo guardamos en un array llamado calibrations
-const input1 = fs.readFileSync('./example.txt', 'utf-8').trim().split('\r\n');
-const input2 = fs.readFileSync('./test.txt', 'utf-8').trim().split('\r\n');
+const input1 = fs.readFileSync('./example.txt', 'utf-8').trim().split('\n');
+const input2 = fs.readFileSync('./test.txt', 'utf-8').trim().split('\n');
 
 console.log("Part 1")
 console.log("Example:", part1(input1));
-//console.log("Test:", part1(input2));
+console.log("Test:", part1(input2));
 
 
 function part1(almanac){
@@ -33,90 +33,89 @@ function part1(almanac){
                 let numbers = almanac[i].split(' ').map(Number);
                 seeds = seeds.map(seed => {
                     if(seed.seed >= numbers[1] && seed.seed < numbers[1]+numbers[2] && !seed.changed){
-                        return {seed: numbers[0] + Math.abs(seed.seed - (numbers[1]+numbers[2])) , changed: true};
+                        return {seed: numbers[0] + (seed.seed - numbers[1]) , changed: true};
                     } else {
                         return seed;
                     }
                 })
-                console.log(i);
-                console.log(seeds);
             }
 
             seeds = seeds.map(seed => {
                 return {seed: seed.seed, changed: false};
             })
-            
-            
+
+        }
+    }
+
+    //Ahora me fijo cuál es la menor
+    let res = seeds[0].seed;
+    for(let seed of seeds){
+        if (res > seed.seed) {
+            res = seed.seed;
+        }
+    }
+
+    return res;
+}
+
+
+console.log('\n',"Part 2")
+console.log("Example:", part2(input1));
+//console.log("Test:", part2(input2));
+
+
+function part2(almanac){
+    let seeds = almanac[0].split(':')[1];
+    seeds = seeds.trim().split(' ');
+    
+    let seed_ranges = [];
+    for(let i = 0; i < seeds.length-1 ;i++){
+        seed_ranges.push({start: Number(seeds[i]), len: Number(seeds[i+1]), changed: false});
+        i++;
+    }
+    
+    for(let i=1; i < almanac.length; i++){
+        if (almanac[i] != '' && isNaN(almanac[i][0])) {
+            //Tenemos categoría 
+            //Ahora guardamos un objeto por cada i siguiente que sea un mapeo
+            for(i=i+1; i < almanac.length && !isNaN(almanac[i][0]); i++){
+                let numbers = almanac[i].split(' ').map(Number);
+
+                for(let range of seed_ranges){
+                    /*Queremos ver qué parte del rango le corresponde a esto, entonces, qué opciones tenemos para que cambie el rango
+                        1. range incluido o igual al rango del almanaque //No se agregan rangos
+                        2. rango corrido a izq //Se divide el rango en 2
+                        3. rango corrido a der
+                        4. rango mayor al rango propuesto
+                    */
+                   //Ahora veamos cómo dividimos nuestro rango de ser necesario
+                    
+                   //El rango original se puede dividir en hasta 3 partes, en peor caso.
+                   //Y para esto necesito entonces 4 "indices"
+                   let min_num, mid_min, mid_max, max_num;
+
+                   if (range.start < numbers[1]) {
+                        min_num = range.start ;
+                        mid_min = numbers[1];
+                   } else {
+                        min_num = numbers[1] ;
+                        mid_min = range.start;
+                   }
+
+                   if (range.start + range.len < numbers[1] + numbers[2]) {
+                        max_num = numbers[1] + numbers[2];
+                        mid_max = range.start + range.len;
+                   } else {
+                        max_num = range.start + range.len;
+                        mid_max = numbers[1] + numbers[2];
+                   }
+
+                   
+                }
+            }
 
         }
     }
 
     return 0;
-}
-
-
-/* console.log('\n',"Part 2")
-console.log("Example:", part2(input1));
-console.log("Test:", part2(input2));
- */
-
-function part2(cards){
-    //Primero mapeamos la cantidad de tarjetas que tenemos
-    let copies = {};
-    for(let i = 1; i <= cards.length ;i++){
-        copies[i] = 1;
-    }
-    
-    for(let card of cards){
-        let id = card.split(':')[0].split(' ');
-        id = Number(id[id.length -1]);
-
-        card = card.split(':')[1].split('|');
-        let win_nums_aux = card[0].trim().split(' ');
-        let my_nums_aux = card[1].trim().split(' ');
-
-        let win_nums = [];
-        for(let num of win_nums_aux){
-            if (num != '') {
-                win_nums.push(Number(num));
-            }
-        }
-
-        let my_nums = [];
-        for(let num of my_nums_aux){
-            if (num != '') {
-                my_nums.push(Number(num));
-            }
-        }
-
-        //Ordenamos de forma ascendente ambos arrays
-        win_nums.sort((a,b) => a-b);
-        my_nums.sort((a,b) => a-b);
-
-        //Y ahora los comparamos para ver a qué potencia de 2 nos encontramos con esta carta
-        let winnings = 0;
-        let i_w = 0; //Index for win_nums
-        let i_m = 0; //Index for my_nums
-
-        while(i_w < win_nums.length && i_m < my_nums.length){
-            if (my_nums[i_m] == win_nums[i_w]) {
-                winnings++;
-                i_m++;
-                i_w++;
-            } else if (my_nums[i_m] > win_nums[i_w]) {
-                i_w++;
-            } else {
-                i_m++;
-            }
-        }
-
-        //Y ahora cambiamos el mapeo de las copias y agregamos a cards
-        for (let i = 1; i <= winnings; i++) {
-            copies[id + i] += copies[id];
-        }
-    }
-    
-    let total = Object.values(copies).reduce((a,b) => a+b);
-
-    return total;
 }
